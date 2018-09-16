1c6fceeac6e6643291e7cf67107800c4dea1409b
ladyada <limor@ladyada.net>
2018-09-16 11:13:03 -0400

    init commit

---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/miniqr_simpletest.py
+    :caption: examples/miniqr_simpletest.py
+    :linenos:
---
+++ examples/miniqr_simpletest.py
@@ -0,0 +1,33 @@
+import sys
+import adafruit_miniqr
+
+# For drawing filled rectangles to the console:
+out = sys.stdout
+WHITE = "\x1b[1;47m  \x1b[40m"
+BLACK = "  "
+
+def print_QR(matrix):
+    # white 4-pixel border at top
+    for _ in range(4):
+        for _ in range(matrix.width+8):
+            out.write(WHITE)
+        print()
+    for y in range(matrix.height):
+        out.write(WHITE*4)   # 4-pixel border to left
+        for x in range(matrix.width):
+            if matrix[x, y]:
+                out.write(BLACK)
+            else:
+                out.write(WHITE)
+        out.write(WHITE*4)   # 4-pixel bporder to right
+        print()
+    # white 4-pixel border at bottom
+    for _ in range(4):
+        for _ in range(matrix.width+8):
+            out.write(WHITE)
+        print()
+
+qr = adafruit_miniqr.QRCode(3, pyqrnative.H)
+qr.addData(b'https://www.adafruit.com')
+qr.make()
+print_QR(qr.matrix)
