cb0e1e8a623f788b292d2fc0b7b54c7961c025d4
dherrada <dylan.herrada@adafruit.com>
2021-01-12 12:55:56 -0500

    Ran pre-commit, added licenses

--- adafruit_miniqr.py
+++ adafruit_miniqr.py
@@ -1,38 +1,17 @@
-# The MIT License (MIT)
+# SPDX-FileCopyrightText: 2009 Kazuhiko Arase
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 #
-#  Ported from the Javascript library by Sam Curren
-#  QRCode for Javascript
-#  http://d-project.googlecode.com/svn/trunk/misc/qrcode/js/qrcode.js
+# SPDX-License-Identifier: MIT
+
+# Ported from the Javascript library by Sam Curren
+# QRCode for Javascript
+# http://d-project.googlecode.com/svn/trunk/misc/qrcode/js/qrcode.js
 #
-#  Copyright (c) 2009 Kazuhiko Arase
-#  URL: http://www.d-project.com/
+# Minimized for CircuitPython by ladyada for adafruit industries
 #
-#  Minimized for CircuitPython by ladyada for adafruit industries
-#
-#  Licensed under the MIT license:
-#    http://www.opensource.org/licenses/mit-license.php
-#
-#  The word "QR Code" is registered trademark of
-#  DENSO WAVE INCORPORATED
-#    http://www.denso-wave.com/qrcode/faqpatent-e.html
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# The word "QR Code" is registered trademark of
+# DENSO WAVE INCORPORATED
+# http://www.denso-wave.com/qrcode/faqpatent-e.html
 """
 `adafruit_miniqr`
 ====================================================
