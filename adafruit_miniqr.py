3d3b85df9641c88f62ab10a81003f29ffbdef7c3
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_miniqr.py
+++ adafruit_miniqr.py
@@ -36,7 +36,7 @@ Implementation Notes
 # imports
 import math
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_miniQR.git"
 
 # Consts!
